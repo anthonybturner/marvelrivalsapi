4804eddffb89202a4ef45b2e4eb3e9bd0194a549
Anthony <anthonybturner@icloud.com>
2025-08-30 00:51:40 -0400

    Fix: Remove HttpsRedirects trailing slash

--- MarvelRivalsApi/Program.cs
+++ MarvelRivalsApi/Program.cs
@@ -25,7 +25,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowAngularApp", policy =>
     {
         policy.WithOrigins(
-            "http://localhost:4200", "https://marvelrivalsui.vercel.app/"    // Local dev
+            "http://localhost:4200", "https://marvelrivalsui.vercel.app"    // Local dev
         )
         .AllowAnyHeader()
         .AllowAnyMethod();
