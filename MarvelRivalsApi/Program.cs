f7e4507ab12e3d3aa28e4f297f17e3427f292217
Anthony <anthonybturner@icloud.com>
2025-08-30 00:46:05 -0400

    Fix: Remove HttpsRedirects

--- MarvelRivalsApi/Program.cs
+++ MarvelRivalsApi/Program.cs
@@ -103,7 +103,6 @@ app.UseSwaggerUI(c =>
 });
 
 // --- Middlewares ---
-app.UseHttpsRedirection();
 app.UseRouting();
 app.UseCors("AllowAngularApp");  // <-- Must be BEFORE UseAuthorization
 app.UseAuthorization();
